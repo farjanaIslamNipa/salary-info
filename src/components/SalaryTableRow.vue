c7cbb73219f6920c35617b46b736ff2cf3c9be9c
farjanaIslam09 <farjana.jatri@gmail.com>
2023-03-23 15:31:56 +0600

    Calculation of total taxable amount completed

--- src/components/SalaryTableRow.vue
+++ src/components/SalaryTableRow.vue
@@ -1,42 +1,42 @@
 <template>
     <td class="px-3 py-2 text-sm w-2/12">
-        <span>{{ salary.month }}</span><span>&nbsp;{{ new Date().getFullYear() }}</span>
+        <span>{{ salary.month }}</span><span>&nbsp;{{ (new Date().getFullYear()) - 1 }}</span>
     </td>
-    <td class="text-sm font-light px-2 py-2 capitalize">
+    <td class="common-td">
         <input v-model.trim="salaryInfo.grossSalary" type="number" class="bg-transparent focus:outline-none py-2 border border-gray-500 rounded-sm px-3 text-right">
     </td>
-    <td class="text-sm font-light px-2 py-2 capitalize">
-        <div class="flex justify-center border border-gray-500 rounded-sm w-full">
-        <input v-model.trim="salaryInfo.basicPercentage" type="number" class="bg-transparent focus:outline-none bg-[#1a2e44] py-2 px-3 w-[40%]">
-        <input v-model="percentage" readonly type="text" class="focus:outline-none py-2 bg-gray-600 w-[20%] text-center">
-        <input v-model="basicSalary" type="number" class="bg-transparent focus:outline-none py-2 bg-[#1a2e44] px-3 w-[40%] text-right">
+    <td class="common-td">
+        <div class="input-group-wrapper">
+        <input v-model.trim="salaryInfo.basicPercentage" type="number" class="common-input">
+        <input v-model="percentage" readonly type="text" class="percentage-input">
+        <input v-model="salaryInfo.basicSalary" readonly type="number" class="common-input">
         </div>
     </td>
-    <td class="text-sm font-light px-2 py-2 capitalize">
-        <div class="flex justify-center border border-gray-500 rounded-sm w-full">
-        <input v-model.trim="salaryInfo.homeRentPercentage" type="number" class="bg-transparent focus:outline-none bg-[#1a2e44] py-2 px-3 w-[40%]">
-        <input v-model="percentage" readonly type="text" class="focus:outline-none py-2 bg-gray-600 w-[20%] text-center">
-        <input v-model="homeRent" type="number" class="bg-transparent focus:outline-none py-2 bg-[#1a2e44] px-3 w-[40%] text-right">
+    <td class="common-td">
+        <div class="input-group-wrapper">
+        <input v-model.trim="salaryInfo.homeRentPercentage" type="number" class="common-input">
+        <input v-model="percentage" readonly type="text" class="percentage-input">
+        <input v-model="salaryInfo.homeRent" readonly type="number" class="common-input">
         </div>
     </td>
-    <td class="text-sm font-light px-2 py-2 capitalize">
-        <div class="flex justify-center border border-gray-500 rounded-sm w-full">
-        <input v-model.trim="salaryInfo.medicalPercentage" type="number" class="bg-transparent focus:outline-none bg-[#1a2e44] py-2 px-3 w-[40%]">
-        <input v-model="percentage" readonly type="text" class="focus:outline-none py-2 bg-gray-600 w-[20%] text-center">
-        <input v-model="medicalAllowance" type="number" class="bg-transparent focus:outline-none py-2 bg-[#1a2e44] px-3 w-[40%] text-right">
+    <td class="common-td">
+        <div class="input-group-wrapper">
+        <input v-model.trim="salaryInfo.medicalPercentage" type="number" class="common-input">
+        <input v-model="percentage" readonly type="text" class="percentage-input">
+        <input v-model="salaryInfo.medicalAllowance" readonly type="number" class="common-input">
         </div>
     </td>
-    <td class="text-sm font-light px-2 py-2 capitalize">
-        <div class="flex justify-center border border-gray-500 rounded-sm w-full">
-        <input v-model.trim="salaryInfo.conveyancePercentage" type="number" class="bg-transparent focus:outline-none bg-[#1a2e44] py-2 px-3 w-[40%]">
-        <input v-model="percentage" readonly type="text" class="focus:outline-none py-2 bg-gray-600 w-[20%] text-center">
-        <input v-model="salaryInfo.conveyanceAllowance" type="number" class="bg-transparent focus:outline-none py-2 bg-[#1a2e44] px-3 w-[40%] text-right">
+    <td class="common-td">
+        <div class="input-group-wrapper">
+        <input v-model.trim="salaryInfo.conveyancePercentage" type="number" class="common-input">
+        <input v-model="percentage" readonly type="text" class="percentage-input">
+        <input v-model="salaryInfo.conveyanceAllowance" readonly type="number" class="common-input">
         </div>
     </td>
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 
 const percentage = ref('%')
 
@@ -47,44 +47,20 @@ const salaryInfo = ref({})
 
 onMounted(() => {salaryInfo.value = salary})
 
-// Basic Salary
-const basicSalary = computed(() => {
-    let getBasicSalary = 0;
-    if(salaryInfo.value.grossSalary){
-        getBasicSalary = Math.round(salaryInfo.value.grossSalary * salaryInfo.value.basicPercentage / 100)  
-    }else{
-       getBasicSalary = ''
+watch(salary, currentSalary => {
+    if(currentSalary.grossSalary && currentSalary.grossSalary > 0){
+        salaryInfo.value.basicSalary =  Math.round(salaryInfo.value.grossSalary * salaryInfo.value.basicPercentage / 100)
+        salaryInfo.value.homeRent =  Math.round(salaryInfo.value.grossSalary * salaryInfo.value.homeRentPercentage / 100)
+        salaryInfo.value.medicalAllowance =  Math.round(salaryInfo.value.grossSalary * salaryInfo.value.medicalPercentage / 100)
+        salaryInfo.value.conveyanceAllowance =  Math.round(salaryInfo.value.grossSalary * salaryInfo.value.conveyancePercentage / 100)
+    }else {
+        salaryInfo.value.basicSalary = ''
+        salaryInfo.value.homeRent = ''
+        salaryInfo.value.medicalAllowance = ''
+        salaryInfo.value.conveyanceAllowance = ''
     }
-    return getBasicSalary
 })
 
-// Home Rent
-const homeRent = computed(() => {
-    let getHomeRent = 0;
-    getHomeRent = Math.round(salaryInfo.value.grossSalary * salaryInfo.value.homeRentPercentage / 100)
-    return getHomeRent
-})
-
-// Medical Allowance
-const medicalAllowance = computed(() => {
-    let getMedicalAllowance = 0;
-    getMedicalAllowance = Math.round(salaryInfo.value.grossSalary * salaryInfo.value.medicalPercentage / 100)
-    return getMedicalAllowance
-})
-
-// watch(salary, currentSalary => {
-//     if(currentSalary.grossSalary && currentSalary.grossSalary > 0){
-//         salaryInfo.value.basicSalary =  Math.round(salaryInfo.value.grossSalary * salaryInfo.value.basicPercentage / 100)
-//         salaryInfo.value.homeRent =  Math.round(salaryInfo.value.grossSalary * salaryInfo.value.homeRentPercentage / 100)
-//         salaryInfo.value.medicalAllowance =  Math.round(salaryInfo.value.grossSalary * salaryInfo.value.medicalPercentage / 100)
-//         salaryInfo.value.conveyanceAllowance =  Math.round(salaryInfo.value.grossSalary * salaryInfo.value.conveyancePercentage / 100)
-//     }else {
-//         salaryInfo.value.basicSalary = ''
-//         salaryInfo.value.homeRent = ''
-//         salaryInfo.value.medicalAllowance = ''
-//         salaryInfo.value.conveyanceAllowance = ''
-//     }
-// })
 
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="pt-5 pb-10 app-primary-bg app-primary-text h-screen">
-    <div class="grid grid-cols-12 gap-4 px-20">
+    <div class="grid grid-cols-12 gap-4 lg:px-20 px-5">
       <div class="col-span-9">
         <div class="app-secondary-bg">
           <div class="py-1 bg-gold capitalize">
             <h3 class="text-xl font-bold text-center text-gray-700">
               salary information
-              {{ testCal }}
             </h3>
           </div>
-          <div class="">
-            <table class="min-w-full">
+          <div class="overflow-x-auto">
+            <table class="w-[1000px] xl:w-full">
               <colgroup>
                 <col span="1" style="width: 16%" />
                 <col span="1" style="width: 12%" />
@@ -21,18 +20,22 @@
               </colgroup>
               <thead class="app-primary-bg">
                 <tr>
-                  <th scope="col" class="text-sm font-bold px-2 py-2 text-left">Month</th>
-                  <th scope="col" class="text-sm font-bold px-2 py-2 text-left">
+                  <th scope="col" class="footer-left-align-td">
+                    Month
+                  </th>
+                  <th scope="col" class="footer-left-align-td">
                     Gross Salary
                   </th>
-                  <th scope="col" class="text-sm font-bold px-2 py-2 text-left">Basic</th>
-                  <th scope="col" class="text-sm font-bold px-2 py-2 text-left">
+                  <th scope="col" class="footer-left-align-td">
+                    Basic
+                  </th>
+                  <th scope="col" class="footer-left-align-td">
                     Home rent
                   </th>
-                  <th scope="col" class="text-sm font-bold px-2 py-2 text-left">
+                  <th scope="col" class="footer-left-align-td">
                     Medical
                   </th>
-                  <th scope="col" class="text-sm font-bold px-2 py-2 text-left">
+                  <th scope="col" class="footer-left-align-td">
                     Conveyance
                   </th>
                 </tr>
@@ -41,62 +44,80 @@
                 <tr
                   v-for="salary in salaryData"
                   :key="salary.id"
-                  class="border-b border-gray-500"
+                  class="border-b border-gray-600"
                 >
                   <SalaryTableRow :salary="salary" />
                 </tr>
               </tbody>
               <tfoot>
                 <tr class="bg-blue-500">
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-left">Total</td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">
+                  <td scope="col" class="footer-left-align-td">Total</td>
+                  <td scope="col" class="footer-right-align-td">
                     {{ getSum.totalGrossSalary }}
                   </td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">
+                  <td scope="col" class="footer-right-align-td">
                     {{ getSum.totalBasicSalary }}
                   </td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">
+                  <td scope="col" class="footer-right-align-td">
                     {{ getSum.totalHomeRent }}
                   </td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">
+                  <td scope="col" class="footer-right-align-td">
                     {{ getSum.totalMedicalAllowance }}
                   </td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">
+                  <td scope="col" class="footer-right-align-td">
                     {{ getSum.totalConveyanceAllowance }}
                   </td>
                 </tr>
                 <tr class="bg-blue-900">
-                  <td colspan="2" class="text-sm font-bold px-2 py-2 text-left">
+                  <td colspan="2" class="footer-left-align-td">
                     Tax Exemption
                   </td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">-0</td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">
+                  <td scope="col" class="footer-right-align-td">0</td>
+                  <td scope="col" class="footer-right-align-td">
                     {{ homeRentTaxExemption }}
                   </td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">
+                  <td scope="col" class="footer-right-align-td">
                     {{ medicalTaxExemption }}
                   </td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">
+                  <td scope="col" class="footer-right-align-td">
                     {{ conveyanceTaxExemption }}
                   </td>
                 </tr>
                 <tr class="bg-gray-600">
-                  <td colspan="2" class="text-sm font-bold px-2 py-2 text-left">
+                  <td colspan="2" class="footer-left-align-td">
                     Taxable
                   </td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">-0</td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">
-                    -30000
+                  <td scope="col" class="footer-right-align-td">{{ taxableBasicSalary }}</td>
+                  <td scope="col" class="footer-right-align-td">
+                    {{ taxableHomeRent }}
                   </td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">
-                    10000
+                  <td scope="col" class="footer-right-align-td">
+                    {{ taxableMedicalAllowance }}
                   </td>
-                  <td scope="col" class="text-sm font-bold px-2 py-2 text-right">5000</td>
+                  <td scope="col" class="footer-right-align-td">{{ taxableConveyanceAllowance }}</td>
                 </tr>
               </tfoot>
             </table>
           </div>
         </div>
+        <div class="mt-6">
+        <div class="flex justify-between items-center">
+            <div class="flex gap-3 items-center">
+            <div>
+                <label class="text-sm block" for="">Festive Bonus One</label>
+                <input v-model="festiveBonusOne" type="number" class="block focus:outline-none app-secondary-bg py-1 px-3 border border-gray-500 rounded-sm">
+            </div>
+            <div>
+                <label class="text-sm block" for="">Festive Bonus Two</label>
+                <input v-model="festiveBonusTwo" type="number" class="block focus:outline-none app-secondary-bg py-1 px-3 border border-gray-500 rounded-sm">
+            </div>
+            </div>
+            <div>
+            <p class="pr-2 font-semibold"><span class="text-lg text-blue-400">Total Taxable Amount :&nbsp;</span> <span class="text-xl">{{ totalTaxableAmount }}</span></p>
+            </div>
+        </div>
+      </div>
+        <!-- <TotalTaxableSection /> -->
       </div>
       <div class="col-span-3">
         <div class="app-secondary-bg p-6">
@@ -111,9 +132,12 @@
 <script setup>
 import sourceData from "../../data.json";
 import SalaryTableRow from "@/components/SalaryTableRow.vue";
+// import TotalTaxableSection from "@/components/TotalTaxableSection.vue";
 import { ref, computed } from "vue";
 
 const salaryData = ref([...sourceData]);
+const festiveBonusOne = ref('')
+const festiveBonusTwo = ref('')
 
 const getSum = computed(() => {
   let totalGrossSalary = 0;
@@ -123,11 +147,13 @@ const getSum = computed(() => {
   let totalConveyanceAllowance = 0;
   
   for(let i = 0; i< salaryData.value.length; i++) {
-    totalGrossSalary += salaryData.value[i].grossSalary
-    totalBasicSalary += salaryData.value[i].basicSalary
-    totalHomeRent += salaryData.value[i].homeRent
-    totalMedicalAllowance += salaryData.value[i].medicalAllowance
-    totalConveyanceAllowance += salaryData.value[i].conveyanceAllowance
+    if(salaryData.value[i].grossSalary > 0){
+      totalGrossSalary += salaryData.value[i].grossSalary
+      totalBasicSalary += salaryData.value[i].basicSalary
+      totalHomeRent += salaryData.value[i].homeRent
+      totalMedicalAllowance += salaryData.value[i].medicalAllowance
+      totalConveyanceAllowance += salaryData.value[i].conveyanceAllowance
+    }
   }
 
   return {
@@ -139,7 +165,8 @@ const getSum = computed(() => {
   }
 })
 
-// Calculation Home Rent Tax Exemption 
+// Calculation of Tax Exemption 
+// Home Rent
 const homeRentTaxExemption = computed(() => {
   let getHomeRentTaxExemption = Math.round(getSum.value.totalBasicSalary * 50 / 100)
       if(getHomeRentTaxExemption < 300000){
@@ -149,7 +176,7 @@ const homeRentTaxExemption = computed(() => {
       }
 })
 
-// Calculation Medical Tax Exemption 
+// Medical
 const medicalTaxExemption = computed(() => {
   let getMedicalTaxExemption = Math.round(getSum.value.totalBasicSalary * 10 / 100)
       if(getMedicalTaxExemption < 120000){
@@ -159,14 +186,66 @@ const medicalTaxExemption = computed(() => {
       }
 })
 
-// Calculation Conveyance Exemption 
+// Conveyance 
 const conveyanceTaxExemption = computed(() => {
-  let getConveyanceTaxExemption = Math.round(getSum.value.totalBasicSalary * 5 / 100)
-      if(getConveyanceTaxExemption < 30000){
-         return getConveyanceTaxExemption
+      if(getSum.value.totalConveyanceAllowance < 30000){
+         return 0
       }else{
-        return 30000
+        return 30000 
       }
+})
+
+// Calculation of Taxable Amount
+// Basic Salary
+const taxableBasicSalary = computed(() => {
+  let taxableBasic = 0
+  if(getSum.value.totalBasicSalary){
+    taxableBasic =  getSum.value.totalBasicSalary - 0
+  }
+  return taxableBasic;
+})
+
+// Home Rent
+const taxableHomeRent = computed(() => {
+  return getSum.value.totalHomeRent - homeRentTaxExemption.value
+})
+
+// Medical
+const taxableMedicalAllowance = computed(() => {
+  return getSum.value.totalMedicalAllowance - medicalTaxExemption.value
+})
+
+// Conveyance
+const taxableConveyanceAllowance = computed(() => {
+  return getSum.value.totalConveyanceAllowance - conveyanceTaxExemption.value
+})
+
+// Calculation of Total Taxable Amount
+const totalTaxableAmount = computed(() => {
+  let getTotalTaxableAmount = 0
+  if(taxableBasicSalary.value){
+    getTotalTaxableAmount = taxableBasicSalary.value +
+    taxableHomeRent.value +
+    taxableMedicalAllowance.value +
+    taxableConveyanceAllowance.value +
+    (+festiveBonusOne.value) +
+    (+festiveBonusTwo.value)
+  }
+
+  if((festiveBonusOne.value && festiveBonusOne.value > 0) || (festiveBonusTwo.value && festiveBonusTwo.value > 0)){
+    getTotalTaxableAmount = taxableBasicSalary.value +
+                              taxableHomeRent.value +
+                              taxableMedicalAllowance.value +
+                              taxableConveyanceAllowance.value +
+                              (+festiveBonusOne.value) +
+                              (+festiveBonusTwo.value)
+    }else{
+      getTotalTaxableAmount = taxableBasicSalary.value +
+                              taxableHomeRent.value +
+                              taxableMedicalAllowance.value +
+                              taxableConveyanceAllowance.value 
+    }
+  return getTotalTaxableAmount
 })
 
 
